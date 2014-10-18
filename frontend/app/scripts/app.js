060cfc97e57b42dce65081e26bcafe946026b702
terracotaPie <timur.borkhodoev@mail.utoronto.ca>
2014-10-17 20:24:54 -0400

    added calendar view and directive

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.calendar'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -26,6 +27,10 @@ angular
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
       })
       .otherwise({
         redirectTo: '/'
---
+++ frontend/app/scripts/controllers/dashboard.js
@@ -0,0 +1,9 @@
+//
+
+angular.module('frontendApp')
+    .controller('DashboardCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'This is your calendar'
+        ];
+        $scope.eventSources = [];
+    });
