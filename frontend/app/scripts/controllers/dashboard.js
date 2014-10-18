f68c0ba4892c991affb18b929e8ddad02da48437
terracotaPie <timur.borkhodoev@mail.utoronto.ca>
2014-10-18 02:28:14 -0400

    added docs for calendar

--- frontend/app/scripts/controllers/dashboard.js
+++ frontend/app/scripts/controllers/dashboard.js
@@ -1,4 +1,8 @@
-//
+/*
+Documentation for Calendar:
+ http://fullcalendar.io/docs/
+ https://github.com/angular-ui/ui-calendar
+ */
 
 angular.module('frontendApp')
     .controller('DashboardCtrl', function ($scope) {
