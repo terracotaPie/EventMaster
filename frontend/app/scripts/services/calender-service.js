ee7c623dcac2f05808374f5ed9c1731180126371
Rakshit Shukla <shuklapossible@gmail.com>
2014-10-26 18:34:46 -0400

    Commented out empty Calendar service to get rid of Grunt warnings.

--- frontend/app/scripts/services/calender-service.js
+++ frontend/app/scripts/services/calender-service.js
@@ -7,12 +7,12 @@
 
 	- Handle RESTful operations for calender
 	- Loading dynamic data on calender for every user, calling calender API, modification etc.
-*/
+
 
 angular.module('frontendApp')
 
   .factory('calender', function($resource, $q, server) {
-    /*
-      Calender API methods go here.
-    */
+
   });
+
+  */
