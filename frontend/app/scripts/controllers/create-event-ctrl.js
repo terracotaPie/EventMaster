3f43f1c9beb7e1a66fb623bed0ca807c0f679017
Rakshit Shukla <shuklapossible@gmail.com>
2014-10-27 21:56:44 -0400

    Added create-event view and controller

---
+++ frontend/app/scripts/controllers/create-event-ctrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:CreateCtrl
+ * @description
+ * # CreateCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('CreateCtrl', function () {
+
+  });
